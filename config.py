0cc8b58d0ed453e39f54309da8488c07ea90479c
gzapodea <gzapodea@cisco.com>
2018-04-08 14:56:13 -0700

    updated with all files for workshop

---
+++ config.py
@@ -0,0 +1,22 @@
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+# This file contains the Spark Auth, ServiceNow Dev account info, router info
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Put your Spark Token here'
+SPARK_ROOM = 'Config Change Alert' + 'your name'
+SPARK_MEMBER = 'gzapodea@cisco.com'
+
+SNOW_URL = 'https://your instance.service-now.com/api/now'
+SNOW_USER = 'your user'
+SNOW_PASS = 'your password'
+SNOW_INSTANCE = 'your instance'
+
+HOST = '10.1.1.1'
+USER = 'cisco'
+PASS = 'cisco_1234!'
+PORT = '830'
+
+EXT_HOST = '10.10.20.48'
+
---
+++ netconf_restconf.py
@@ -0,0 +1,44 @@
+
+
+# developed by Gabi Zapodeanu, TSA, Global Partner Organization
+
+
+import requests
+import json
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.auth import HTTPBasicAuth  # for Basic Auth
+from config import HOST, PORT, USER, PASS, EXT_HOST
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # Disable insecure https warnings
+
+
+ROUTER_AUTH = HTTPBasicAuth(USER, PASS)
+
+def pprint(json_data):
+
+    print(json.dumps(json_data, indent=4, separators=(' , ', ' : ')))
+
+def get_restconf_int_oper_data(interface):
+
+    url = 'https://' + EXT_HOST + '/restconf/data/interfaces-state/interface=' + interface
+    print(url)
+    header = {'Content-type': 'application/yang-data+json', 'accept': 'application/yang-data+json'}
+    response = requests.get(url, headers=header, verify=False, auth=ROUTER_AUTH)
+    interface_info = response.json()
+    oper_data = interface_info['ietf-interfaces:interface']
+    return oper_data
+
+
+def get_restconf_hostname():
+
+    url = 'https://' + EXT_HOST + '/restconf/data/Cisco-IOS-XE-native:native/hostname'
+    header = {'Content-type': 'application/yang-data+json', 'accept': 'application/yang-data+json'}
+    response = requests.get(url, headers=header, verify=False, auth=ROUTER_AUTH)
+    hostname_json = response.json()
+    hostname = hostname_json['Cisco-IOS-XE-native:hostname']
+    return hostname
+
+pprint(get_restconf_int_oper_data('GigabitEthernet1'))
+
+print(get_restconf_hostname())
