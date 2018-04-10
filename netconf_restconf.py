579a1977a2dddb58df4cb908b8d615430f23408d
gzapodea <gzapodea@cisco.com>
2018-04-10 07:34:28 -0700

    updated netconf_restconf

--- netconf_restconf.py
+++ netconf_restconf.py
@@ -6,6 +6,7 @@
 import requests
 import json
 
+
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 from requests.auth import HTTPBasicAuth  # for Basic Auth
 from config import HOST, PORT, USER, PASS, EXT_HOST
@@ -18,6 +19,7 @@ ROUTER_AUTH = HTTPBasicAuth(USER, PASS)
 def pprint(json_data):
 
     print(json.dumps(json_data, indent=4, separators=(' , ', ' : ')))
+
 
 def get_restconf_int_oper_data(interface):
 
@@ -42,3 +44,91 @@ def get_restconf_hostname():
 pprint(get_restconf_int_oper_data('GigabitEthernet1'))
 
 print('Device Hostname via RESTCONF: ', get_restconf_hostname())
+
+"""
+This section is an example of NETCONF Requests, DO NOT CHANGE THIS!
+
+import ncclient
+import xml
+import xml.dom.minidom
+
+from ncclient import manager
+
+def get_netconf_int_oper_status(interface):
+    """
+This
+function
+will
+retrieve
+the
+IPv4
+address
+configured
+on
+the
+interface
+via
+NETCONF
+:param
+interface: interface
+name
+:return: int_ip_add: the
+interface
+IPv4
+address
+"""
+
+with manager.connect(host=HOST, port=PORT, username=USER,
+                     password=PASS, hostkey_verify=False,
+                     device_params={'name': 'default'},
+                     allow_agent=False, look_for_keys=False) as m:
+    # XML filter to issue with the get operation
+    # IOS-XE 16.6.2+        YANG model called "ietf-interfaces"
+
+    interface_state_filter = '''
+                                <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                                    <interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+                                        <interface>
+                                            <name>''' + interface + '''</name>
+                                            <oper-status/>
+                                        </interface>
+                                    </interfaces-state>
+                                </filter>
+                            '''
+
+    result = m.get(interface_state_filter)
+    xml_doc = xml.dom.minidom.parseString(result.xml)
+    int_info = xml_doc.getElementsByTagName('oper-status')
+    try:
+        oper_status = int_info[0].firstChild.nodeValue
+    except:
+        oper_status = 'unknown'
+    return oper_status
+
+def get_netconf_hostname():
+with manager.connect(host=HOST, port=PORT, username=USER,
+                     password=PASS, hostkey_verify=False,
+                     device_params={'name': 'default'},
+                     allow_agent=False, look_for_keys=False) as m:
+    # XML filter to issue with the get operation
+    # IOS-XE 16.6.2+        YANG model called "Cisco-IOS-XE-native"
+
+    hostname_filter = '''
+                            <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                                <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                                    <hostname/>
+                                </native>
+                            </filter>
+                      '''
+
+    result = m.get(hostname_filter)
+    xml_doc = xml.dom.minidom.parseString(result.xml)
+    int_info = xml_doc.getElementsByTagName('hostname')
+    try:
+        hostname = int_info[0].firstChild.nodeValue
+    except:
+        hostname = 'unknown'
+    return hostname
+
+"""
+
