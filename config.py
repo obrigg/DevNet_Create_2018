d68930b7bf8ad392b685f4fb16713bead0187354
Oren Brigg <50162672+obrigg@users.noreply.github.com>
2019-10-17 14:10:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,16 @@
-
 # developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
 
-# This file contains the Spark Auth, ServiceNow Dev account info, router info
+# This file contains the Spark Auth, network device info
 
 SPARK_URL = 'https://api.ciscospark.com/v1'
 SPARK_AUTH = 'Bearer ' + 'Put your Spark Token here'
 SPARK_ROOM = 'Config Change Alert ' + 'your name'
 SPARK_MEMBER = 'gzapodea@cisco.com'
 
-SNOW_URL = 'https://dev48476.service-now.com/api/now'
-SNOW_USER = 'user'
-SNOW_PASS = 'password'
-SNOW_INSTANCE = 'dev48476'
-
-HOST = '10.10.20.48'
-USER = 'cisco'
-PASS = 'cisco_1234!'
+#dCloud general credentials
+USER = 'netadmin'
+PASS = 'C1sco12345!'
 PORT = '830'
 
-EXT_HOST = '10.10.20.48'
+HOST = '99.99.99.102'
+EXT_HOST = '99.99.99.102'
