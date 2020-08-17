4386c444d0d0df49d89f37bee39cf1a778da0c64
Oren Brigg <obrigg@cisco.com>
2020-08-17 20:20:17 +0300

    updated config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 WEBEX_TEAMS_ACCESS_TOKEN = 'Put your Spark Token here, without the Bearer'
 WEBEX_TEAMS_ROOM = 'Config Change Alert ' + 'your name'
-WEBEX_TEAMS_MEMBERS = ['obrigg@cisco.com']
-PROXY = {'https': 'http://proxy.esl.cisco.com:80'}
+WEBEX_TEAMS_MEMBERS = ['your@email.com']
+PROXY = {'https': 'http://proxy.if.relevant.com:80'}
